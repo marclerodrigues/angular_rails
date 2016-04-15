51a5206c9cc3c454770b0d759be7d1c07eedcf02
Marcle Rodrigues <maarclee@gmail.com>
2016-04-15 10:08:54 -0300

    Model Specs Finished

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :todo do
-    
+   title "Title" 
+   description "Description"
   end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:todo) { FactoryGirl.create(:todo) }
+  it 'has a valid factory' do
+    expect(todo).to be_valid
+  end
+
+  it 'is invalid without title' do
+    todo.title = nil
+    expect(todo).not_to be_valid
+  end
+
+  it 'is valid without description' do
+    todo.description = nil
+    expect(todo).to be_valid
+  end
 end
