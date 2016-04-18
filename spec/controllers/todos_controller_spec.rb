cae27d91d6df57cd78e7392f4afa8d4fca49071a
Marcle Rodrigues <maarclee@gmail.com>
2016-04-17 22:07:58 -0300

    Controller#show specs

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe TodosController, type: :controller do
       expect(assigns(:todos)).to eq([@todo])
     end
   end
+
+  describe 'GET #show' do
+    before :each do
+      @todo = FactoryGirl.create(:todo)
+      get :show, id: @todo
+    end
+
+    it 'returns success' do
+      expect(response).to be_success
+    end
+
+    it 'assigns correctly @todo' do
+      expect(assigns(:todo)).to eq(@todo)
+    end
+  end
 end
