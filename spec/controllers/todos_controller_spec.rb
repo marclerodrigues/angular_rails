29658e0c914abc1d7fcf982ac33bc83643131d74
Marcle Rodrigues <maarclee@gmail.com>
2016-04-17 22:04:57 -0300

    Controller#index spec

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe TodosController, type: :controller do
+  describe 'GET #index' do
+    before :each do
+      @todo = FactoryGirl.create(:todo)
+      get :index
+    end
 
+    it 'returns success' do
+      expect(response).to be_success
+    end
+
+    it 'assigns correctly @todos' do
+      expect(assigns(:todos)).to eq([@todo])
+    end
+  end
 end
